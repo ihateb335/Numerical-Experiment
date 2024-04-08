8e8f14e4890df093e5f323706b49c43030d0a9d7
MrZahn <mykhailo.zhar@stud.onu.edu.ua>
2024-04-08 13:17:44 +0300

    Add Lab 04

---
+++ NE_DLL/NE_Rectangular_Plate.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NE_DLL
+{
+    public class NE_Rectangular_Plate
+    {
+        readonly double T1, T2, T3, T4, a, b;
+        readonly int k1, k2, k3, k4;
+        readonly double a1_1, a1_2, bt_3, bt_4;
+
+        public NE_Rectangular_Plate(
+            double a, double b, 
+            double T1, double T2, double T3, double T4,
+            int k1, int k2, int k3, int k4)
+        {
+            this.T1 = T1;
+            this.T2 = T2;
+            this.T3 = T3;
+            this.T4 = T4;
+            this.a = a;
+            this.b = b;
+            this.k1 = k1;
+            this.k2 = k2;
+            this.k3 = k3;
+            this.k4 = k4;
+
+            a1_1 = Math.PI * k1 / a;
+            a1_2 = Math.PI * k2 / a;
+            bt_3 = Math.PI * k3 / b;
+            bt_4 = Math.PI * k4 / b;
+        }
+
+        private double GetT(double exp1, double exp2, double exp3) 
+            => Math.Exp(-exp1) * (1.0 - Math.Exp(-2.0 * exp2)) / (1.0 - Math.Exp(-2.0 * exp3)) ;
+
+        public double GetTemperatureAtPoint(double x, double y)
+        {
+            // формули (20) -(23)
+            double
+                t1 = GetT(a1_1 * y, a1_1 * (b - y), a1_1 * b ),
+                t2 = GetT(a1_2 * (b - y), a1_2 * y, a1_2 * b ),
+                t3 = GetT(bt_3 * x, bt_3 * (a - x), bt_3 * a ),
+                t4 = GetT(bt_4 * (a - x), bt_4 * x, bt_4 * a );
+
+            // формула (24)
+            t1 *= T1 * Math.Sin(a1_1 * x);
+            t2 *= T2 * Math.Sin(a1_2 * x);
+            t3 *= T3 * Math.Sin(bt_3 * y);
+            t4 *= T4 * Math.Sin(bt_4 * y);
+
+            return t1 + t2 + t3 + t4;
+        }
+
+    }
+}
---
+++ NE_LW_04/NE_LW_04.cs
@@ -0,0 +1,63 @@
+﻿using System.IO;
+using RP = NE_DLL.NE_Rectangular_Plate;
+
+namespace NE_LW_04
+{
+    internal class NE_LW_04
+    {
+        static void Main(string[] args)
+        {
+            //Test data
+            WriteData(
+                30.0, 20.0, -15.0, 15.0, //T
+                2.0, 1.0, // a, b
+                5, 3, 1, 2, // k 1 - 4
+                1.0, 0.2, 1.5, 0.75, //Points
+                "Nodes_LW_04.txt", "Map_LW_04.txt"
+            );
+            //Variant 5
+            WriteData(
+                20.0, 5.0, 25.0, -10.0, //T
+                2.0, 1.0, // a, b
+                2, 5, 6, 1, // k 1 - 4
+                .3, .55, 1.25, 0.65, //Points
+                "Nodes_LW_04_Variant_05.txt", "Map_LW_04_Variant_05.txt", 200
+            );
+        }
+
+        static private void WriteData(
+            double T1, double T2, double T3, double T4, 
+            double a, double b,
+            int k1, int k2, int k3, int k4,
+            double xc1, double yc1, double xc2, double yc2,
+            string nodeFileName, string mapFileName, int m = 400
+        )
+        {
+           
+            RP plate = new RP(a, b, T1, T2, T3, T4, k1, k2, k3, k4);
+
+            StreamWriter writer = new StreamWriter(nodeFileName);
+            double t1 = plate.GetTemperatureAtPoint(xc1, yc1), t2 = plate.GetTemperatureAtPoint(xc2, yc2);
+            writer.WriteLine($" t1 = {t1,18:F8}     t2 = {t2,18:F8}");
+            writer.Close();
+
+            writer = new StreamWriter(mapFileName);
+            int i, j, M = (int)(a * m), N = (int)(b * m);
+            double h = 1.0 / m, x = 0.0, y = 0.0;
+            for (j = 0; j <= N; j++)
+            {
+
+                y = h * j;
+                for (i = 0; i <= M; i++)
+                {
+
+
+                    x = h * i; t1 = plate.GetTemperatureAtPoint(x, y);
+                    writer.WriteLine($" {x,10:F6}   {y,10:F6}  {t1,12:F8}".Replace(',', '.'));
+                }
+            }
+            writer.Close();
+        }
+
+    }
+}
